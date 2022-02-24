40a51cc6fd8405f4a46a113f8b23fb161b41ae4b
LucieGuitton <lucie.guitton@outlook.fr>
2022-02-24 16:28:53 +0100

    debut modif DAOLogement

--- src/main/java/dao/DAOLogement.java
+++ src/main/java/dao/DAOLogement.java
@@ -1,21 +1,54 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Logement;
+
 
 public class DAOLogement implements IDAO<Logement, Integer> {
 
 	@Override
 	public Logement findById(Integer id) {
 
-		return null;
+		Logement l = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+			PreparedStatement ps = conn.prepareStatement("SELECT * from logement where numero=?");
+			ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()) 
+			{
+               //à compléter
+			}
+
+			rs.close();
+			ps.close();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return l;
 	}
 
 	@Override
 	public List<Logement> findAll() {
 
-		return null;
+		List<Logement> logements = new ArrayList();
+		
+		//à compléter
+		
+		return logements;
 	}
 
 	@Override
