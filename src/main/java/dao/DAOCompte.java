89696c103afc71d6a8d2261d41faeba79d1ab408
Chaymae <chaymae.sati@gmail.com>
2022-02-25 12:04:43 +0100

    DAOcompte

--- src/main/java/dao/DAOCompte.java
+++ src/main/java/dao/DAOCompte.java
@@ -1,15 +1,61 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.List;
 
+import model.Client;
 import model.Compte;
+import model.Hote;
+import model.Organisateur;
+import model.Reservation;
+import model.TypeDePaiement;
 
 public class DAOCompte implements IDAO<Compte, Integer> {
 
 	@Override
 	public Compte findById(Integer id) {
+		Compte c = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+			PreparedStatement ps = conn.prepareStatement("SELECT * from compte where id_compte=?");
+			ps.setInt(1, id);
 
-		return null;
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()) 
+			{
+				if(rs.getString("type_compte").equals("client")) 
+				{
+					c = new Client(rs.getInt("id_compte"),rs.getString("nom"),rs.getString("prenom"),rs.getString("mail"),rs.getString("password"),rs.getDate("dateNaissance"));
+
+	
+				}
+				else if (rs.getString("type_compte").equals("Organisateur"))
+				{
+					c = new Organisateur(rs.getInt("id_compte"),rs.getString("nom"),rs.getString("prenom"),rs.getString("mail"),rs.getDate("dateNaissance"));
+					
+				}
+				else if (rs.getString("type_compte").equals("Hote"))
+				{
+					c = new Hote(rs.getInt("id_compte"),rs.getString("nom"),rs.getString("prenom"),rs.getString("mail"),rs.getDate("dateNaissance"));
+					
+				}
+			}
+			
+
+			rs.close();
+			ps.close();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return c;
 	}
 
 	@Override
