b9d114b3f44854990141993ba0ee31f3b93fa2e6
LucieGuitton <lucie.guitton@outlook.fr>
2022-03-08 12:00:44 +0100

    Ajout dependance JPA

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,26 @@
 package test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+
+
+		/*
+		EntityManagerFactory emf  = Persistence.createEntityManagerFactory("monroadtripUnit");
+
+		EntityManager em  = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		em.close();
+		emf.close();
+		*/
 
 	}
 
